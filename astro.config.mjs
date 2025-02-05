6702dfd288ff96ee52e237f8eab06295740c5bad
zjwqsd <221502006@smail.nju.edu.cn>
2025-02-05 10:51:14 +0800

    search

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,13 @@ import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+// import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pushpatnap.github.io',
-
+  build: {
+    format: "file",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
